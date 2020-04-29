99e2781a3f0ed35d26fb8a829bc36ba4c3b90acf
Hitesh Kumar Saini <57103779+hit7sh@users.noreply.github.com>
2020-04-29 17:54:12 +0530

    Create MACROS.h

---
+++ MACROS.h
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define speed() ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define eb(n) emplace_back(n)
+#define debug(x) cout<<#x<<" is "<<x<<endl
+#define endl "\n"
+#define pb(n) push_back(n)
+#define pf(n) push_front(n)
+#define f0(i,n) for(i = 0; i < n; i++)
+#define f1(i,n) for(i = 1; i <= n; i++)
+#define fo(i, start, end) for(i = start; i <= end; i++)
+#define fro(i, start, end) for(i = start; i >= end; i--)
+#define foreach(i,t) for(typeof(t.begin()) i = t.begin(); i!=t.end(); i++)
+#define foreachrev(i,t) for(typeof(t.begin()) i = t.rbegin(); i!=t.rend(); i++)
+using namespace std;
+using ll = long long;
+using umii = unordered_map<int, int>;
+using mii = map<int, int>;
+using vi = vector<int>;
+using vl = vector<ll>;
+const int mod=1e9+7;
