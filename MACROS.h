93702a1ce58a8c1d3254daa908d71f1c9fdceb3a
Hitesh Kumar Saini <hit7sh@gmail.com>
2021-07-09 22:24:29 +0530

    Update MACROS.h

--- MACROS.h
+++ MACROS.h
@@ -1,49 +1,82 @@
 // Author :: <Hitesh_Saini>
 #include<bits/stdc++.h>
+ 
+#define __speed() cin.tie(0)->sync_with_stdio(0), cin.exceptions(cin.failbit);
 
-#define __speed() ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
-#define dbg(x) cout << "[" << __LINE__ << ": "<< #x << " = " << x << " ]\n";
-#define tt int t;for(cin>>t; t--; )
+#ifndef ONLINE_JUDGE
+#define dbg(...) write(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define dbg(...) 42
+#endif
 
-#define f0(i, n) for(i = 0; i < (int)(n); i++)
-#define f1(i, n) for(i = 1; i<= (int)(n); i++)
-
-#define cmax(x, y) x=max(x, y);
-#define cmin(x, y) x=min(x, y);
-
+#define Yes(x) print((x) ? "Yes" : "No")
+#define TC     int t; for (cin >> t; t--; )
+ 
+#define f0(i, n)  for (i = 0; i < (int)(n); i++)
+#define f1(i, n)  for (i = 1; i <=(int)(n); i++)
+ 
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define sz(x) (int)(x.size())
-
+ 
 #define EB emplace_back
 #define PB push_back
+
+#define LB lower_bound
+#define UB upper_bound
 
 #define endl "\n"
 #define S second
 #define F first
 
-
 using namespace std;
 using mii = map<int, int>;
 using pii = pair<int, int>;
-
-using ll = long long;
+ 
+using ll = int64_t;
 using vi = vector<int>;
-
+ 
 using vvi = vector<vi>;
 using vl = vector<ll>;
+ 
+const pair<int, int> DD[] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+const int mod = 1e9 + 7, mxN = 2e5 + 5, INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+ 
 
-const int mod = 1e9+7, mxN = 2e5+5;
+
+template <typename... T> void print(T... args) { ((cout << args << " "), ...), cout << endl; }
+template <typename... T> void write(string vars, T... args) { cout << endl; cout << "[ "; cout << vars << " = "; string delim = ""; (..., (cout << delim << args, delim = ", ")); cout << "]"; cout << endl; }
+
+template <typename T1, typename T2> istream& operator>>(istream& in, pair<T1, T2>& p) { in >> p.F >> p.S; return in; }
+template <typename T1, typename T2> ostream& operator<<(ostream& ot, pair<T1, T2>& p) { ot << p.F << ' ' << p.S; return ot; }
+
+template <typename T1, typename T2> bool cmax(T1& a, T2 b) { if (b > a) { a = b; return true; } return false; }
+template <typename T1, typename T2> bool cmin(T1& a, T2 b) { if (b < a) { a = b; return true; } return false; }
+
+template <typename T> istream& operator>>(istream& in, vector<T>& v) { for (T& x:v) in >> x; return in; }
+template <typename T> ostream& operator<<(ostream& ot, vector<T>& v) { for (T& x:v) ot << x << ' '; return ot;}
+
+template <typename T> ostream& operator<<(ostream& ot, set<T>& s) { for (T x : s) ot << x << ' '; return ot; }
+
+ll mod_pow(ll x, ll y){ll res = 1;x = x % mod;while(y>0){if(y&1)res = (res*x) % mod; y = y>>1;x = (x*x) % mod; }return res; }ll mod_inv ( ll n ){ll ans = mod_pow ( n , mod - 2 ) ;return ans ;}ll mul(ll a, ll b){a%=mod, b%=mod;return (a*b)%mod;}ll sub(ll a,ll b){a%=mod, b%=mod;return (a-b+mod)%mod;}ll add(ll a, ll b){a%=mod, b%=mod;return (a+b)%mod;}
 
 
-inline void Yes(bool e){cout << (e?"Yes\n":"No\n");}inline void yes(bool e){cout << (e?"yes\n":"no\n");}inline void YES(bool e){cout << (e?"YES\n":"NO\n");}
-ll mod_pow(ll x, ll y){ll res=1;x%=mod;for(;y>0; y>>=1, x=(x*x)%mod)if(y&1) res=(res*x) % mod; return res; }ll mod_inv ( ll n ){ll ans = mod_pow ( n , mod - 2 ); return ans ;}inline ll mul(ll a, ll b){a%=mod, b%=mod;return (a*b)%mod;}ll sub(ll a,ll b){a%=mod, b%=mod;return (a-b+mod)%mod;}inline ll add(ll a, ll b){a%=mod, b%=mod; return (a+b)%mod;}
+#define random(a, b) uniform_int_distribution<int>(a, b)(rng) // random number [a, b]
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 
-template<typename... Args> void print(Args... args) { ((cout << args << " "), ...); cout << endl; }
 
-template<typename T1,typename T2> istream& operator>>(istream& in,pair<T1,T2> &a) { in>>a.F>>a.S;return in; }
-template<typename T1,typename T2> ostream& operator<<(ostream& out,pair<T1,T2> a) { out<<a.F<<" "<<a.S;return out; }
 
-template<typename T> istream& operator>>(istream& in,vector<T> &a) { for(auto&x:a)in>>x;return in; }
-template<typename T> ostream& operator<<(ostream& out,vector<T> a) { for(auto&x:a)out << x << ' ';return out; }
+void solve() {
+  
+} 
+
+
+
+main() { 
+  cout << fixed << setprecision(6);  
+
+  __speed() TC solve();
+  // int t, i; for (cin >> t, i = 1; i <= t; i++) cout << "Case #" << i << ": ", solve();
+}
